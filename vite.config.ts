cdd3d2410fc93084ab1c01d9b44ca21775082eb2
yugansh-ahuja <yugansh.ahuja@si-eam.com>
2026-09-17 14:30:06 +0530

    chore: keep local wdir on original base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/assets-common-prototype-new/',
+  base: '/assets-common-prototype/',
   server: { port: 5173 },
 });
