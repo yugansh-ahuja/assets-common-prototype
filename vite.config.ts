2d09a76b74d3e96a65f92d28e9c28b4c4869db56
yugansh-ahuja <yugansh.ahuja@si-eam.com>
2026-09-17 14:22:49 +0530

    assets-common-prototype-new

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/assets-common-prototype/',
+  base: '/assets-common-prototype-new/',
   server: { port: 5173 },
 });
